83799878485c46bdab73283497de2e0bcc6e7e03
QuanCachan <mqtran.cachan@gmail.com>
2021-02-18 22:33:07 +0100

    add login and settings vues

--- src/vues/login/Authentication.vue
+++ src/vues/login/Authentication.vue
@@ -1,36 +1,40 @@
 <template>
-  <div>
-    Login Page
-    <div>
-      <ul>
-        <li v-for="i in getMaleChar" :key="i.id">
-          {{i.name}}
-        </li>
-      </ul>
-      <div>
-        <h4>Count: {{count}}</h4>
-        <button class="btn btn-primary" v-on:click="increment(4)">Add</button>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <a href="">Have an account?</a>
+          </p>
+
+          <ul class="error-messages">
+            <li>That email is already taken</li>
+          </ul>
+
+          <form>
+            <fieldset class="form-group">
+              <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+            </fieldset>
+            <fieldset class="form-group">
+              <input class="form-control form-control-lg" type="text" placeholder="Email">
+            </fieldset>
+            <fieldset class="form-group">
+              <input class="form-control form-control-lg" type="password" placeholder="Password">
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign up
+            </button>
+          </form>
+        </div>
+
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: "login",
-  computed: {
-    getMaleChar() {
-      return this.$store.getters.maleChars;
-    },
-    count() {
-      return this.$store.state.count;
-    }
-  },
-  methods: {
-    increment(payload) {
-      //this.$store.commit("increment");
-      this.$store.dispatch('increment', payload);
-    }
-  },
-  mounted() {}
+  name: 'login'
 }
 </script>
